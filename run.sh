2177d6e6b25cf3d0fbedd863af8a6ddb33837e81
TAKAHASHI Yuto <ytaka23dev@gmail.com>
2018-05-30 02:29:02 +0900

    Add the setup command before build

--- run.sh
+++ run.sh
@@ -8,11 +8,13 @@ SERVER_INSTALL_ROOT=$(stack path --local-install-root ${SERVER_STACK_OPTION})
 
 SERVER_STATIC_DIR=server/static
 
+stack setup ${CLIENT_STACK_OPTION}
 stack build ${CLIENT_STACK_OPTION}
 
 mkdir -p ${SERVER_STATIC_DIR}
 cp ${CLIENT_INSTALL_ROOT}/bin/miso-tutorial-app-client.jsexe/all.js \
    ${SERVER_STATIC_DIR}/all.js
 
+stack setup ${SERVER_STACK_OPTION}
 stack build ${SERVER_STACK_OPTION}
 cd server && ${SERVER_INSTALL_ROOT}/bin/miso-tutorial-app-server
