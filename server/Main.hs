1e7709943bc68b0e4d94c7d26d7e8cb8edb4ec38
TAKAHASHI Yuto <ytaka23dev@gmail.com>
2018-06-08 21:30:07 +0900

    Show logs in the correct order

--- server/Main.hs
+++ server/Main.hs
@@ -3,12 +3,14 @@ module Main where
 import           Control.Concurrent.STM
 import           Network.Wai.Handler.Warp
 import           Network.Wai.Logger
+import           System.IO
 
 import           Server
 
 main :: IO ()
 main = do
     initialDB <- newTVarIO initialPlayers
+    hSetBuffering stdout LineBuffering
     putStrLn "Listening on port 4000..."
     withStdoutLogger $ \logger -> do
         let settings = setPort 4000 $ setLogger logger defaultSettings
